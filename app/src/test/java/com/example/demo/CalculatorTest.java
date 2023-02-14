3d0fd7104a62a1e767325864f613c36a20d2bdb8
Frank <f.berrocalazofeif887@mybvc.ca>
2023-02-14 10:13:43 -0600

    UI Completed
    Style included,  and constructions of operand buttons.

---
+++ app/src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import androidx.test.filters.SmallTest;
+
+import com.example.demo.Calculator;
+
+/**
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
+ */
+@RunWith(JUnit4.class)
+@SmallTest
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
+    }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+
+
+}
